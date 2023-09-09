2ca2b5c21b8df93174586038880028f40c3d7443
tanvirtbf <tanver018765@gmail.com>
2023-09-09 16:13:11 +0600

    first commit

---
+++ App.js
@@ -0,0 +1,38 @@
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
+
+const faqs = document.querySelectorAll('.FAQ1');
+faqs.forEach((faq) => {
+  faq.addEventListener("click", () => {
+    faq.classList.toggle("active");
+  })
+})
+
+
+const showPassword = document.querySelector("#show-password");
+const passwordField = document.querySelector("#password");
+showPassword.addEventListener("click",function(){
+  this.classList.toggle("fa-eye");
+  const type = passwordField.getAttribute("type") === "password" ? "text" : "password";
+  passwordField.setAttribute("type",type)
+})
+
+const according = document.querySelectorAll('.contentBx');
+
+for(i = 0; i<according.length; i++){
+  according[i].addEventListener('click',function(){
+    this.classList.toggle("active");
+  })
+}
